3822fe09c9dbfcbf342f4edc2b534b2b6171bc12
jadhavarjun <jadhavarjun53@gmail.com>
2023-12-12 21:44:08 +0530

    implemented feature: search and movie will be apear

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const api = "https://www.omdbapi.com/?apikey=ced7ed6e&t=";
+let notFound = document.getElementById("notFound");
+let found = document.getElementById("found");
 
 function searchMovie() {
   let movieName = document.getElementById("movieName");
@@ -12,10 +14,23 @@ function searchMovie() {
       console.log(data);
       if (data.Error) {
         notFound.classList.remove("d-none");
+        found.classList.add("d-none");
         // notFound.classList.add("d-none");
         document.getElementById("name").innerText = movieName.value;
       } else {
         notFound.classList.add("d-none");
+        found.classList.remove("d-none");
+        document.getElementById("title").innerText = data.Title;
+        document.getElementById("poster").src = data.Poster;
+        document.getElementById("genre").innerText = data.Genre;
+        document.getElementById("date").innerText = data.Released;
+        document.getElementById("actors").innerText = data.Actors;
+        document.getElementById("collection").innerText = data.BoxOffice;
+        document.getElementById("awards").innerText = data.Awards;
+        document.getElementById("director").innerText = data.Director;
+        document.getElementById("writer").innerText = data.Writer;
+        document.getElementById("ratings").innerText = data.imdbRating;
+        document.getElementById("desc").innerText = data.Plot;
       }
     });
 }
